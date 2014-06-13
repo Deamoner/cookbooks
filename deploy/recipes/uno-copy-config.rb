90760e11340fb27f0e1e58e6b353373648e5610b
mackybautista <macky@virtustructure.com>
2014-06-14 01:39:10 +0800

    uno copy config

--- deploy/recipes/uno-copy-config.rb
+++ deploy/recipes/uno-copy-config.rb
@@ -1,5 +1,4 @@
 bash "uno_copy_config" do
   user "root"
-  app_name = node[:opsworks][:applications][:name] 
-  command "echo '#{app_name}' > /tmp/appname"
+  command "echo 'node[:opsworks][:applications][:name]' > /tmp/appname"
 end
