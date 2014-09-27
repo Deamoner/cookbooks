fe84b21644da7668dc2b2cc47a15d65d38312351
Macky Bautista <macky@virtustructure.com>
2014-09-27 15:35:41 +0800

    peak-change_permissions

--- deploy/recipes/peak_change_permissions.rb
+++ deploy/recipes/peak_change_permissions.rb
@@ -1,8 +1,11 @@
-appname = node[:deploy]['appshortname'][:application];
-bash "change_permissions" do
-  user "root"
-  code <<-EOH
-  cd /srv/www/#{appname}
-  chmod -Rf 777 current/*
-  EOH
+node[:deploy].each do |application, deploy|
+    deploy = node[:deploy][application];
+	appname = deploy[:deploy_to];
+	bash "change_permissions" do
+	  user "root"
+	  code <<-EOH
+	  cd /srv/www/#{appname}
+	  chmod -Rf 777 current/*
+	  EOH
+	end
 end
