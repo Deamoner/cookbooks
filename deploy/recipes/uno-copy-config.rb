d979fe891377c469ae84f32bd2f4f50fb6531f69
Macky Bautista <macky@virtustructure.com>
2014-10-20 02:28:45 +0800

    config update

--- deploy/recipes/uno-copy-config.rb
+++ deploy/recipes/uno-copy-config.rb
@@ -1,4 +1,18 @@
-bash "uno_copy_config" do
-  user "root"
-  	command "echo node[:opsworks][:applications][:name] > /tmp/appname"
+node[:deploy].each do |application, deploy|
+    deploy = node[:deploy][application];
+	appname = deploy[:deploy_to];
+	bash "change_permissions" do
+	  user "root"
+	  code <<-EOH
+	  cd /srv/www/#{appname}
+	  yes | cp -Rf current/OpenVBX/config/specifics/prod/database.php current/OpenVBX/config/
+	  yes | cp -Rf current/OpenVBX/config/specifics/prod/openvbx.php current/OpenVBX/config/
+	  yes | cp -Rf current/OpenVBX/config/specifics/prod/config.php current/OpenVBX/config/
+	  yes | cp -Rf current/OpenVBX/config/specifics/prod/stripe.php current/OpenVBX/config/
+	  yes | cp -Rf current/OpenVBX/config/specifics/prod/uno.php current/OpenVBX/config/
+	  yes | cp -Rf current/OpenVBX/config/specifics/prod/.htaccess current/
+	  chmod -Rf 777 current/*
+	  EOH
+	end
 end
+
--- deploy/recipes/uno-email-deploy.rb
+++ deploy/recipes/uno-email-deploy.rb
@@ -1,7 +1,10 @@
-appname = node[:opsworks][:applications][i][:name];
-bash "email_deploy" do
-  user "root"
-  code <<-EOH
-  echo "#{appname} Deploy" | mail -s "Uno.im AutoDeploy" helpdesk@virtustructure.com
-  EOH
+node[:deploy].each do |application, deploy|
+    deploy = node[:deploy][application];
+	appname = deploy[:deploy_to];
+	bash "change_permissions" do
+	  user "root"
+	  code <<-EOH
+	  echo "#{appname} Deploy" | mail -s "Uno.im AutoDeploy" helpdesk@virtustructure.com
+	  EOH
+	end
 end
