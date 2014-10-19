76a869cd4529708707e46dd38740b71f4f1915c3
Macky Bautista <macky@virtustructure.com>
2014-10-20 02:58:21 +0800

    config fix

--- deploy/recipes/uno-copy-config.rb
+++ deploy/recipes/uno-copy-config.rb
@@ -5,12 +5,12 @@ node[:deploy].each do |application, deploy|
 	  user "root"
 	  code <<-EOH
 	  cd #{appname}
-	  yes | cp -Rf current/OpenVBX/config/specifics/prod/database.php current/OpenVBX/config/
-	  yes | cp -Rf current/OpenVBX/config/specifics/prod/openvbx.php current/OpenVBX/config/
-	  yes | cp -Rf current/OpenVBX/config/specifics/prod/config.php current/OpenVBX/config/
-	  yes | cp -Rf current/OpenVBX/config/specifics/prod/stripe.php current/OpenVBX/config/
-	  yes | cp -Rf current/OpenVBX/config/specifics/prod/uno.php current/OpenVBX/config/
-	  yes | cp -Rf current/OpenVBX/config/specifics/prod/.htaccess current/
+	  yes | cp -Rf current/OpenVBX/config/specifics/#{appname}/database.php current/OpenVBX/config/
+	  yes | cp -Rf current/OpenVBX/config/specifics/#{appname}/openvbx.php current/OpenVBX/config/
+	  yes | cp -Rf current/OpenVBX/config/specifics/#{appname}/config.php current/OpenVBX/config/
+	  yes | cp -Rf current/OpenVBX/config/specifics/#{appname}/stripe.php current/OpenVBX/config/
+	  yes | cp -Rf current/OpenVBX/config/specifics/#{appname}/uno.php current/OpenVBX/config/
+	  yes | cp -Rf current/OpenVBX/config/specifics/#{appname}/.htaccess current/
 	  chmod -Rf 777 current/*
 	  EOH
 	end
