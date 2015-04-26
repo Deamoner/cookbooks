b8010508812f78d0399a9596f744c7d67e21bafb
Macky Bautista <macky@virtustructure.com>
2015-04-27 02:14:13 +0800

    Update rms-restore-uploads-prod.rb

--- deploy/recipes/rms-restore-uploads-prod.rb
+++ deploy/recipes/rms-restore-uploads-prod.rb
@@ -3,6 +3,7 @@ bash "restore_uploads_prod" do
   cwd "/srv/www/rms_production"
   code <<-EOH
   sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+  chmod -Rf 777 current/assets
   yes | cp -Rf backup/temp/* current/assets/uploads/*
   EOH
 end
