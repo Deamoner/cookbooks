c158ff317057b5ca196c27250a7efcb3fabb130a
Macky Bautista <macky@virtustructure.com>
2015-07-08 15:00:54 +0800

    mysql host update

--- deploy/recipes/rms-db-synch.rb
+++ deploy/recipes/rms-db-synch.rb
@@ -2,10 +2,10 @@ bash "db_synch" do
   user "root"
   cwd "/srv/www/rms_production"
   code <<-EOH
-  mysqldump -u rms -pjfYbLDmq4T3EA4dv -h 107.20.215.51 rms_production_old > /tmp/rms_production_old.sql
-  mysqladmin -u pms_dev -pstagingpms123! -h 107.20.215.51 drop pms_dev --force
-  mysqladmin -u pms_dev -pstagingpms123! -h 107.20.215.51 create pms_dev --force
-  mysql -u pms_dev -pstagingpms123! -h 107.20.215.51 pms_dev < /tmp/rms_production_old.sql
+  mysqldump -u rms -pjfYbLDmq4T3EA4dv -h 54.86.199.105 rms_production_old > /tmp/rms_production_old.sql
+  mysqladmin -u pms_dev -pstagingpms123! -h 54.86.199.105 drop pms_dev --force
+  mysqladmin -u pms_dev -pstagingpms123! -h 54.86.199.105 create pms_dev --force
+  mysql -u pms_dev -pstagingpms123! -h 54.86.199.105 pms_dev < /tmp/rms_production_old.sql
   yes | rm /tmp/rms_production_old.sql
   EOH
 end
